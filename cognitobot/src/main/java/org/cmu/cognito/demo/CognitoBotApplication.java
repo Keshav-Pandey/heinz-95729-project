8d2261b400895f922e8cd414f1a7895d31f6c852
Keshav Pandey <keshav25@outlook.com>
2019-11-21 09:34:46 -0500

    Boiler plate for spring boot

---
+++ cognitobot/src/main/java/org/cmu/cognito/demo/CognitoBotApplication.java
@@ -0,0 +1,13 @@
+package org.cmu.cognito.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoBotApplication.class, args);
+	}
+
+}
